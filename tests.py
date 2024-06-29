70fa2e7ecc8ab8a115b5fdd24f20f7c1bb6519b9
kunai <neni23@mail.ru>
2024-06-29 18:51:01 +0300

    init commit for tests

---
+++ tests.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from unittest import TestCase
+import bowling_engine
+
+
+class BowlingTest(TestCase):
+    """
+    Тесты для модуля bowling_engine
+    """
+
+    def Test_1(self):
+        pass
